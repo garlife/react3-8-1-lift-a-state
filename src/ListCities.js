d6d8beef00514b81b303534113fe083ab93fc178
garlife <garlife@gmail.com>
2022-02-18 00:08:55 +0300

    done with bug

--- src/ListCities.js
+++ src/ListCities.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function ListCities(props) {
   return (
     <ul>
-      {/* {props.cities.map((city, index) => (
+      {props.cities.map((city, index) => (
         <li key={city.name} onClick={() => props.onSelectCity(index)}>
           {city.name} ({city.description})
+          {console.log(city.name, city.description)}
         </li>
-      ))} */}
-      {props.cities.map((city) =><li key={city.name}>{city.name} ({city.description})</li> )}
+      ))}
     </ul>
   );
 }
